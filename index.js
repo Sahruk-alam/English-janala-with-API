231643b3b91e5e9ea132ef3a772c99a5a22446ea
Sahruk alam sarder <sahrukalam8059@gmail.com>
2025-09-16 18:14:24 +0600

    lesson word add

--- index.js
+++ index.js
@@ -9,16 +9,30 @@ const loadLevelWord = (id) => {
     .then(response => response.json())
     .then(data => displayLoad(data.data))
 }
-const displayLoad=(word) => {
+const displayLoad=(word) => { 
     const wordContainer = document.getElementById('word-container');
     wordContainer.innerHTML = "";
+    if(word.length===0){
+        wordContainer.innerHTML=`
+        
+        <div class=" p-6 text-center col-span-3 rounded-xl space-y-4">
+      <img class="mx-auto" src="./assets/alert-error.png" alt="">
+        <p class="text-md">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+      <h2 class="text-4xl font-medium">নেক্সট Lesson এ যান</h2>
+     </div>
+        `
+    return;
+    }
     word.forEach(w => {
+        console.log(w);
         const wordDiv = document.createElement('div');
         wordDiv.innerHTML = `
          <div class="bg-white text-center py-10 px-5 rounded-lg shadow-md">
-        <h2 class="text-2xl font-bold">Eager</h2>
+        <h2 class="text-2xl font-bold">${w.word ? w.word : "No Word Available"}</h2>
         <p class="text-lg font-medium">Meaning /Pronounciation</p>
-        <div class="text-2xl font-semibold font-bangla">"আগ্রহী / ইগার"</div>
+        <div class="text-2xl font-semibold font-bangla">
+        ${w.meaning ? w.meaning : "No Meaning Available"} /
+         ${w.pronunciation ? w.pronunciation : "No Pronunciation Available"}</div>
         <div class="flex justify-between items-center">
           <button class="btn bg-[#1A91FF20] hover:bg-[#1A91FF90]"><i class="fa-solid fa-circle-info"></i></button>
           <button class="btn bg-[#1A91FF20] hover:bg-[#1A91FF90]"><i class="fa-solid fa-volume-high"></i></button>
