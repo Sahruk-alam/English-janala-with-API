e6372af3596de6ecfc1c1e29a191d3a361188332
Sahruk alam sarder <sahrukalam8059@gmail.com>
2025-09-16 14:12:52 +0600

    lesson button and click dynamic

---
+++ index.js
@@ -0,0 +1,51 @@
+const loadlessons = () => {
+fetch('https://openapi.programming-hero.com/api/levels/all')
+.then(response => response.json())
+.then(d => displaylessons(d.data))
+};
+const loadLevelWord = (id) => {
+    const url=`https://openapi.programming-hero.com/api/level/${id}`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => displayLoad(data.data))
+}
+const displayLoad=(word) => {
+    const wordContainer = document.getElementById('word-container');
+    wordContainer.innerHTML = "";
+    word.forEach(w => {
+        const wordDiv = document.createElement('div');
+        wordDiv.innerHTML = `
+         <div class="bg-white text-center py-10 px-5 rounded-lg shadow-md">
+        <h2 class="text-2xl font-bold">Eager</h2>
+        <p class="text-lg font-medium">Meaning /Pronounciation</p>
+        <div class="text-2xl font-semibold font-bangla">"আগ্রহী / ইগার"</div>
+        <div class="flex justify-between items-center">
+          <button class="btn bg-[#1A91FF20] hover:bg-[#1A91FF90]"><i class="fa-solid fa-circle-info"></i></button>
+          <button class="btn bg-[#1A91FF20] hover:bg-[#1A91FF90]"><i class="fa-solid fa-volume-high"></i></button>
+        </div>
+      </div>
+        `
+        wordContainer.appendChild(wordDiv);
+});
+
+
+}
+const displaylessons = (lessons) => {
+    // 1.get contaoner & clear 
+    const lessonContainer = document.getElementById('section-container');
+    lessonContainer.innerHTML = "";
+    // 2.get into every lesson 
+    lessons.forEach(lesson => {
+        // 3.create element 
+        const lessonDiv = document.createElement('div');
+        lessonDiv.innerHTML = `
+         <button onclick="loadLevelWord(${lesson.level_no})" class="text-[#422AD5]  hover:text-white font-semibold 
+         btn btn-outline btn-primary  border-1 p-5 rounded-[5px]">
+          <i class="fa-solid fa-book-open"></i>Lesson -${lesson.level_no
+            }</button>  
+        `;
+        // 4.append container 
+        lessonContainer.appendChild(lessonDiv);
+    });
+}
+loadlessons();
